d83c5e9eef126df8c157422af20686b59732e1ec
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-06-27 22:00:04 +0200

    refactor(components): destructure image-item props

--- src/components/ImageItem.tsx
+++ src/components/ImageItem.tsx
@@ -5,8 +5,17 @@ interface ImageItemProps {
     isFeatured: boolean;
 }
 
-export const ImageItem = (props: ImageItemProps) => {
+// export const ImageItem = (props: ImageItemProps) => {
+//     return (
+//         <img id={props.image.id} src={props.image.src} alt={props.image.alt} className={props.isFeatured ? 'featured' : ''}/>
+//     )
+// }
+
+
+// React sigue pasando el objeto props internamente.
+// JavaScript desestructura el objeto props automáticamente en el parámetro. El objeto sigue existiendo, simplemente no se ve.
+export const ImageItem = ({image, isFeatured}: ImageItemProps) => {
     return (
-        <img id={props.image.id} src={props.image.src} alt={props.image.alt} className={props.isFeatured ? 'featured' : ''}/>
+        <img id={image.id} src={image.src} alt={image.alt} className={isFeatured ? 'featured' : ''}/>
     )
 }
