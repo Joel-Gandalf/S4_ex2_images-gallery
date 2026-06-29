6ebd8786cc43342a292f10da95844ab0018d605f
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-06-29 21:37:09 +0200

    feat(components): implement image deletion with confirmation

--- src/components/ImageItem.tsx
+++ src/components/ImageItem.tsx
@@ -3,6 +3,7 @@ import type { Image } from "../types/image";
 interface ImageItemProps {
     image: Image;
     isFeatured: boolean;
+    onDelete: (id: string) => void;
 }
 
 // export const ImageItem = (props: ImageItemProps) => {
@@ -14,8 +15,11 @@ interface ImageItemProps {
 
 // React sigue pasando el objeto props internamente.
 // JavaScript desestructura el objeto props automáticamente en el parámetro. El objeto sigue existiendo, simplemente no se ve.
-export const ImageItem = ({image, isFeatured}: ImageItemProps) => {
+export const ImageItem = ({image, isFeatured, onDelete}: ImageItemProps) => {
     return (
-        <img id={image.id} src={image.src} alt={isFeatured ? `Imagen destacada: ${image.alt}` : image.alt} className= {`w-full h-full ${isFeatured ? 'lg:col-span-2 lg:row-span-2' : ''}`}/>
+        <div className= {`w-full h-full ${isFeatured ? 'lg:col-span-2 lg:row-span-2' : ''}`}>
+            <img id={image.id} src={image.src} alt={isFeatured ? `Imagen destacada: ${image.alt}` : image.alt} />
+            <button onClick={() => onDelete(image.id)}>Eliminar</button>
+        </div>
     )
 }
