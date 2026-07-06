85cf8d764d2167cfce9c1a56e66e9dfe2c129d8a
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-07-06 11:38:51 +0200

    refactor(data): extract images list to separate data file

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import type { Image } from "../types/image";
 import { ImageItem } from "./ImageItem";
+import { imagesList } from "@/data/images";
 
 import { DragDropProvider } from "@dnd-kit/react";
 import type { DragEndEvent, DragOverEvent } from "@dnd-kit/react";
@@ -12,21 +13,6 @@ import { useRef } from "react";
 
 import { toast } from "sonner";
 
-
-const imagesList: Image[] = [
-    { id: '1', src: 'https://picsum.photos/id/11/400/300', alt: 'Río entre montañas' },
-    { id: '2', src: 'https://picsum.photos/id/12/400/300', alt: 'Río desembocando en el mar' },
-    { id: '3', src: 'https://picsum.photos/id/14/400/300', alt: 'Cala de rocas' },
-    { id: '4', src: 'https://picsum.photos/id/15/400/300', alt: 'Cascada vista desde un acantilado' },
-    { id: '5', src: 'https://picsum.photos/id/17/400/300', alt: 'Camino adentrandose en el bosque' },
-    { id: '6', src: 'https://picsum.photos/id/18/400/300', alt: 'Espiga de trigo' },
-    { id: '7', src: 'https://picsum.photos/id/19/400/300', alt: 'Tronco de árbol con musgo' },
-    { id: '8', src: 'https://picsum.photos/id/29/400/300', alt: 'Cordillera nevada' },
-    { id: '9', src: 'https://picsum.photos/id/46/400/300', alt: 'Montañas desérticas' },
-    { id: '10', src: 'https://picsum.photos/id/35/400/300', alt: 'Cactus' },
-    { id: '11', src: 'https://picsum.photos/id/82/400/300', alt: 'Flor de cerezo' },
-    { id: '12', src: 'https://picsum.photos/id/70/400/300', alt: 'Camino entre arboles' },
-]
 
 export const Gallery = () => {
     // La sintaxis función(param: Tipo) es para definir el tipo de un parámetro en una función que tú escribes. Pero cuando llamas a una función ya existente como useState, no puedes añadir tipos a sus parámetros.
