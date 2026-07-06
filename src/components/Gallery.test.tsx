1a72d1562bd12e5fddba6e55d33b0cc8018e99ba
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-07-06 19:29:52 +0200

    chore(testing): configure package.json, tsconfig.json and vite.config.ts. Create and configure setupTests.ts

---
+++ src/components/Gallery.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import {Gallery} from './Gallery';
+
+     describe('Gallery Component', () => {
+        it('should render gallery title', () => {
+           render(<Gallery />);
+           const heading = screen.getByText('Image Gallery');
+           expect(heading).toBeInTheDocument();
+        });
